2e6f0083dab96738c55ec74d7654e3a724c56ded
Stephen Asbury <sasbury@sasbury.com>
2013-01-20 23:57:46 -0800

    Initial class commit

---
+++ NumberStyleExample.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+import java.text.*;
+
+public class NumberStyleExample
+{
+    public static void main(String args[])
+    {
+        int[] tests = {-10,-4,0,1,4,5,9,10,22,27,41,49,50,75,91,100,350,450,500,990,1000,1564,4990,10000};
+        int test;
+        Random rand = new Random();
+
+        System.out.println();
+        System.out.println("1-100");
+        for(int i=0;i<101;i++)
+        {
+            test = i;
+            System.out.print(""+test);
+            System.out.print("\t"+NumberStyle.convertToAlpha(test));
+            System.out.print("\t"+NumberStyle.convertToLittleAlpha(test));
+            System.out.print("\t"+NumberStyle.convertToBigRoman(test) );
+            System.out.print("\t"+NumberStyle.convertToLittleRoman(test) );
+            System.out.print("\t"+NumberStyle.convertToEnglish(test) );
+            System.out.println();
+        }
+
+        System.out.println("Semi-random numbers");
+        for(int i=0;i<tests.length;i++)
+        {
+            test = tests[i];
+            System.out.print(""+NumberFormat.getNumberInstance().format(test));
+            System.out.print("\t"+NumberStyle.convertToAlpha(test));
+            System.out.print("\t"+NumberStyle.convertToLittleAlpha(test));
+            System.out.print("\t"+NumberStyle.convertToBigRoman(test) );
+            System.out.print("\t"+NumberStyle.convertToLittleRoman(test) );
+            System.out.print("\t"+NumberStyle.convertToEnglish(test) );
+            System.out.println();
+        }
+
+        System.out.println("Size tests");
+        for(int i=0;i<20;i++)
+        {
+            test = Math.abs(rand.nextInt(Integer.MAX_VALUE));
+            System.out.print(""+NumberFormat.getNumberInstance().format(test));
+            System.out.print(" "+NumberStyle.convertToReadableMemorySize(test) );
+            System.out.print(" "+NumberStyle.convertToEnglish(test) );
+            System.out.println();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 numberstyle_java
 ================
 
-Class to convert numbers to different readable styles, like roman numerals or english
+Class for converting numbers to Roman numeral format, as well as English and data sizes. This class can be used for page numbers, or for an outline.
+
+For example, you can convert 6 to 'F' the sixth letter, or to 'vi' the lower case roman equivalent, or to 'six' the English equivalent. There is a hard limit of 4000 for Roman numerals. Conversions for numbers over 26 result in two letter combinations, so 27 is converted to 'AA' or 'aa'.
+
+Below are a few examples of various formats.
+
+> value - TEXT - text - ROMAN - roman - english
+
+> 6 - F - f - VI - vi - six
+
+> 78 - BZ - bz - LXXVIII - lxxviii - seventy eight
+
+> 350 - ML - ml - CCCL - cccl - three hundred fifty
+
+> 1,564 - BHD - bhd - MDLXIV - mdlxiv - one thousand, five hundred sixty four
+
+The class can also be used to compute numbers as sizes. For example:
+
+> 33,051,900 - 31.521MB
+
+> 1,310,861,241 - 1.221GB
